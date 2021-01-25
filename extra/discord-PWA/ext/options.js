7320eeccfb7ae6cb0846147d3b2a8d859bc92ec5
CodyMKW <CodyMKW@users.noreply.github.com>
2021-01-25 07:04:41 -0500

    Add files via upload

---
+++ extra/discord-PWA/ext/options.js
@@ -0,0 +1,43 @@
+function load_options() {
+    var default_options = {
+        "badge_count": "mentions",
+        "draw_attention_on": "messages",
+        "custom_css": "",
+        "custom_js": "",
+        "custom_title": "DISCORD",
+        "relax_CSP_styles": false
+    };
+    chrome.storage.local.get(default_options, function (items) {
+        for (const [setting, value] of Object.entries(items)) {
+            let node = document.getElementById(setting);
+            if (node.type === 'checkbox') {
+                node.checked = value;
+                node.addEventListener("click", e => {
+                    const checked = e.target.checked;
+                    chrome.storage.local.set({
+                        [e.target.id]: checked
+                    }, function () {
+                        if (chrome.runtime.lastError) {
+							console.log('err');
+                            node.checked = !checked;
+                        }
+                    });
+                });
+            } else {
+                node.value = value;
+                node.addEventListener("input", e => {
+                    const setval = e.target.value;
+                    chrome.storage.local.set({
+                        [e.target.id]: setval
+                    }, function () {
+                        if (chrome.runtime.lastError) {
+
+                        }
+                    });
+                });
+            }
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', load_options);
