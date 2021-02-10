57eba0728bfeb9e407d466693e14893fd19a90f7
CodyMKW <CodyMKW@users.noreply.github.com>
2021-02-09 19:05:59 -0500

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,7 +1,6 @@
 // This is the "Offline copy of assets" service worker
 
 const CACHE = "pwabuilder-offline";
-const QUEUE_NAME = "bgSyncQueue";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -11,16 +10,9 @@ self.addEventListener("message", (event) => {
   }
 });
 
-const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
-  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
-});
-
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE,
-    plugins: [
-      bgSyncPlugin
-    ]
+    cacheName: CACHE
   })
 );
