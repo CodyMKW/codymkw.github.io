1d56ed59e909a35a51494dd6fb6827571018e11c
CodyMKW <CodyMKW@users.noreply.github.com>
2023-07-08 13:47:10 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAbout() {
+    document.getElementById('about').style.display = 'block';
+    document.getElementById('switchfc').style.display = 'none';
+    document.getElementById('socialmedia').style.display = 'none';
+  }
+
+  function showSwitchFC() {
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('switchfc').style.display = 'block';
+    document.getElementById('socialmedia').style.display = 'none';
+  }
+
+  function showSocialMedia() {
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('switchfc').style.display = 'none';
+    document.getElementById('socialmedia').style.display = 'block';
+  }
