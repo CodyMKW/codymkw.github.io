3c68fd734fe0e59442d45face3e8357fdcbfee61
CodyMKW <CodyMKW@users.noreply.github.com>
2023-07-08 17:42:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function showAbout() {
-    document.getElementById('about').style.display = 'block';
-    document.getElementById('switchfc').style.display = 'none';
-    document.getElementById('webapps').style.display = 'none';
-    document.getElementById('socialmedia').style.display = 'none';
-  }
+  document.getElementById('about').style.display = 'block';
+  document.getElementById('switchfc').style.display = 'none';
+  document.getElementById('webapps').style.display = 'none';
+  document.getElementById('socialmedia').style.display = 'none';
+}
 
-  function showSwitchFC() {
-    document.getElementById('about').style.display = 'none';
-    document.getElementById('switchfc').style.display = 'block';
-    document.getElementById('webapps').style.display = 'none';
-    document.getElementById('socialmedia').style.display = 'none';
-  }
+function showSwitchFC() {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('switchfc').style.display = 'block';
+  document.getElementById('webapps').style.display = 'none';
+  document.getElementById('socialmedia').style.display = 'none';
+}
 
-  function showWebApps() {
-    document.getElementById('about').style.display = 'none';
-    document.getElementById('switchfc').style.display = 'none';
-    document.getElementById('webapps').style.display = 'block';
-    document.getElementById('socialmedia').style.display = 'none';
-  }
+function showWebApps() {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('switchfc').style.display = 'none';
+  document.getElementById('webapps').style.display = 'block';
+  document.getElementById('socialmedia').style.display = 'none';
+}
 
-  function showSocialMedia() {
-    document.getElementById('about').style.display = 'none';
-    document.getElementById('switchfc').style.display = 'none';
-    document.getElementById('webapps').style.display = 'none';
-    document.getElementById('socialmedia').style.display = 'block';
-  }
+function showSocialMedia() {
+  document.getElementById('about').style.display = 'none';
+  document.getElementById('switchfc').style.display = 'none';
+  document.getElementById('webapps').style.display = 'none';
+  document.getElementById('socialmedia').style.display = 'block';
+}
