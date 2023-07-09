23fbb7ebc7e1fc3836a6916e28bef142fea971a9
CodyMKW <CodyMKW@users.noreply.github.com>
2023-07-08 22:50:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var clickCount = 0;
+
 function showAbout() {
   document.getElementById('about').style.display = 'block';
   document.getElementById('switchfc').style.display = 'none';
@@ -24,4 +26,17 @@ function showSocialMedia() {
   document.getElementById('switchfc').style.display = 'none';
   document.getElementById('webapps').style.display = 'none';
   document.getElementById('socialmedia').style.display = 'block';
+}
+
+function activateSecretTheme() {
+  var body = document.getElementsByTagName('body')[0];
+  body.classList.toggle('secret-theme');
+}
+
+function handleClick() {
+  clickCount++;
+  if (clickCount === 17) {
+    activateSecretTheme();
+    clickCount = 0; // Reset the click count
+  }
 }
