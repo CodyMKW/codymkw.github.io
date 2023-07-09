dfd06d5620b3fdf28f92f8d61a2ac9f9c6ae8185
CodyMKW <CodyMKW@users.noreply.github.com>
2023-07-08 23:48:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,29 +32,13 @@ function activateSecretTheme() {
   clickCount++;
   
   if (clickCount === 17) {
-    if (!secretThemeActive) {
-      var confirmation = confirm("Warning: This secret theme may include flashing colors or other visual effects that could potentially trigger seizures or other adverse reactions in some individuals. Proceed with caution. Do you want to activate the secret theme?");
-      
-      if (confirmation) {
-        var bodyElement = document.querySelector('body');
-        var themes = ['dark-red', 'dark-blue', 'dark-purple', 'light-pink', 'light-blue', 'light-yellow']; // Add more colors if desired
-        var intervalId;
-        
-        intervalId = setInterval(function() {
-          var randomTheme = themes[Math.floor(Math.random() * themes.length)];
-          bodyElement.className = randomTheme;
-        }, 1000);
-        
-        // Save the interval ID and theme status
-        bodyElement.dataset.intervalId = intervalId;
-        secretThemeActive = true;
-      }
-    } else {
-      var bodyElement = document.querySelector('body');
-      clearInterval(bodyElement.dataset.intervalId);
-      bodyElement.className = '';
-      secretThemeActive = false;
-    }
+    var bodyElement = document.querySelector('body');
+    var themes = ['dark-red', 'dark-blue', 'dark-purple', 'light-pink', 'light-blue', 'light-yellow']; // Add more colors if desired
+    
+    setInterval(function() {
+      var randomTheme = themes[Math.floor(Math.random() * themes.length)];
+      bodyElement.className = randomTheme;
+    }, 1000);
   }
 }
 
