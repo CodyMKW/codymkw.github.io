2a1c0c8c0ec49e2467c5a390f02733342f3a220e
CodyMKW <CodyMKW@users.noreply.github.com>
2023-07-13 15:54:13 +0000

    fixed mistake

--- script.js
+++ script.js
@@ -134,6 +134,7 @@ function activateSecretTheme() {
 
     // Show seizure warning popup
     alert("Seizure Warning: The secret theme may contain flashing lights or rapidly changing colors. If you have photosensitive epilepsy or any similar condition, please disable the secret theme immediately by clicking the profile picture again.");
+  }
 }
 
 // Check the hash value in the URL and show the corresponding section
