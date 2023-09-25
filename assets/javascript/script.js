c6ac9aaa085b7d5cbb2f9d64103301ca3dee7d6d
CodyMKW <CodyMKW@users.noreply.github.com>
2023-09-25 22:07:46 +0000

    Offline PWA fix

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -324,7 +324,7 @@ function changeLocation(url) {
 
 // Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
+  navigator.serviceWorker.register('assets/javascript/service-worker.js')
     .then(registration => console.log('Service Worker registered with scope:', registration.scope))
     .catch(error => console.error('Service Worker registration failed:', error));
 }
