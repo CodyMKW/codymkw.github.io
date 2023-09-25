6129ecbea80cb17932f61e1d974a851d2e08f2a0
CodyMKW <CodyMKW@users.noreply.github.com>
2023-09-25 18:44:45 -0400

    Update service-worker.js

--- assets/javascript/service-worker.js
+++ assets/javascript/service-worker.js
@@ -42,20 +42,27 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
+  console.log('Service Worker: Installing...');
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('Service Worker: Caching files during install');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
 self.addEventListener('fetch', event => {
+  console.log('Service Worker: Fetching...');
+  
   event.respondWith(
     caches.match(event.request).then(response => {
       if (response) {
+        console.log('Service Worker: Found in cache:', event.request.url);
         return response;
       }
 
+      console.log('Service Worker: Fetching resource from the network:', event.request.url);
       return fetch(event.request).then(response => {
         if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
@@ -64,6 +71,7 @@ self.addEventListener('fetch', event => {
         const responseToCache = response.clone();
 
         caches.open(CACHE_NAME).then(cache => {
+          console.log('Service Worker: Caching new resource:', event.request.url);
           cache.put(event.request, responseToCache);
         });
 
@@ -74,6 +82,8 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('activate', event => {
+  console.log('Service Worker: Activating...');
+
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
