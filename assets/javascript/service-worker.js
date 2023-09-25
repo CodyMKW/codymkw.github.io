a65ca6997c8224506d6541bed9b0db2aebeb2cf0
CodyMKW <CodyMKW@users.noreply.github.com>
2023-09-25 22:13:52 +0000

    fix?

--- assets/javascript/service-worker.js
+++ assets/javascript/service-worker.js
@@ -51,28 +51,26 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
-        
-        return fetch(event.request).then(response => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
 
-          const responseToCache = response.clone();
+        const responseToCache = response.clone();
 
-          caches.open(CACHE_NAME)
-            .then(cache => {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
         });
-      })
-    );
+
+        return response;
+      });
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
