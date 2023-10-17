0e8a858e9ec8aa5087004ea2fa5ce44da034d00a
CodyMKW <CodyMKW@users.noreply.github.com>
2023-10-17 08:04:23 +0000

    test

--- assets/javascript/frameselect.js
+++ assets/javascript/frameselect.js
@@ -14,12 +14,12 @@ function mergeImages() {
       frameImage.src = selectedFrame;
 
       frameImage.onload = () => {
-          // Set the canvas size to match the frame size
-          canvas.width = frameImage.width;
-          canvas.height = frameImage.height;
+          // Set the canvas size to 400x400
+          canvas.width = 400;
+          canvas.height = 400;
 
           // Draw the frame on the canvas
-          context.drawImage(frameImage, 0, 0);
+          context.drawImage(frameImage, 0, 0, 400, 400);
 
           // Create an image object for the user-selected image
           const userImage = new Image();
@@ -27,7 +27,7 @@ function mergeImages() {
 
           userImage.onload = () => {
               // Draw the user-selected image on top of the frame
-              context.drawImage(userImage, 0, 0, canvas.width, canvas.height);
+              context.drawImage(userImage, 0, 0, 400, 400);
 
               // Convert the merged image to a data URL
               const mergedImageURL = canvas.toDataURL('image/png');
@@ -36,6 +36,9 @@ function mergeImages() {
               const downloadLink = document.getElementById('downloadLink');
               downloadLink.href = mergedImageURL;
               downloadLink.style.display = 'block';
+
+              // Trigger the download
+              downloadLink.click();
           };
       };
   }
