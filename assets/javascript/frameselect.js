906e660ede18ca823dc6bb3a9f66b363bcedeb26
CodyMKW <CodyMKW@users.noreply.github.com>
2023-10-17 07:47:18 +0000

    test

--- assets/javascript/frameselect.js
+++ assets/javascript/frameselect.js
@@ -8,26 +8,37 @@ function setSelectedFrame(selectedFrame) {
   function saveAvatarFrame() {
     const frameSelection = document.getElementById('frameSelection');
     const selectedFrame = frameSelection.value;
-  
+    
     const avatarFrame = document.getElementById('avatar-frame');
-    avatarFrame.innerHTML = `
-        <img src="assets/images/splatfest/${selectedFrame}" alt="Avatar Frame" class="frame-image">
-        <img src="https://www.gravatar.com/avatar/5127de8a3a295a02ff3fc55bb6b2fabe?s=2048" alt="Cody's Avatar" class="avatar-image">
-    `;
-  
-    localStorage.setItem('selectedFrame', selectedFrame);
-
-      // Add a green success message
-  const successMessage = document.createElement('p');
-  successMessage.textContent = 'Selection successfully saved!';
-  successMessage.style.color = '#00C900';
-  document.getElementById('ChangeSplatfestFrameContent').appendChild(successMessage);
-
-  // Remove the success message after a few seconds
-  setTimeout(() => {
-    successMessage.remove();
-  }, 3000); // 3000 milliseconds (3 seconds)
-
+    const avatarImage = avatarFrame.querySelector('.avatar-image');
+    
+    // Load the selected frame image
+    const frameImage = new Image();
+    frameImage.src = `assets/images/splatfest/${selectedFrame}`;
+    
+    frameImage.onload = function () {
+        const canvas = document.getElementById('avatar-canvas');
+        const context = canvas.getContext('2d');
+        
+        // Set canvas size to match the frame image
+        canvas.width = frameImage.width;
+        canvas.height = frameImage.height;
+        
+        // Draw the frame image
+        context.drawImage(frameImage, 0, 0, canvas.width, canvas.height);
+        
+        // Draw the user's avatar on top
+        const avatarImageWidth = canvas.width / 400; // Adjust this as needed
+        const avatarImageHeight = canvas.height / 400; // Adjust this as needed
+        context.drawImage(avatarImage, 0, 0, avatarImageWidth, avatarImageHeight);
+        
+        // Convert the canvas image to a data URL and set it as the download link href
+        const downloadLink = document.getElementById('download-link');
+        downloadLink.href = canvas.toDataURL('image/png');
+        
+        // Trigger a click on the download link
+        downloadLink.click();
+    };
 }
   
   document.addEventListener('DOMContentLoaded', () => {
