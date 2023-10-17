849da6411d669c196712955599d27be30d037d4c
CodyMKW <CodyMKW@users.noreply.github.com>
2023-10-17 07:54:48 +0000

    test

--- assets/javascript/frameselect.js
+++ assets/javascript/frameselect.js
@@ -1,75 +1,42 @@
-// Function to set the selected frame in the dropdown menu
-function setSelectedFrame(selectedFrame) {
-    const frameSelection = document.getElementById('frameSelection');
-    frameSelection.value = selectedFrame;
+function mergeImages() {
+  const frameSelection = document.getElementById('frameSelection');
+  const selectedFrame = frameSelection.value;
+  
+  const imageSelection = document.getElementById('imageSelection');
+  const selectedImage = imageSelection.files[0];
+
+  if (selectedImage) {
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+
+      // Create an image object for the frame
+      const frameImage = new Image();
+      frameImage.src = selectedFrame;
+
+      frameImage.onload = () => {
+          // Set the canvas size to match the frame size
+          canvas.width = frameImage.width;
+          canvas.height = frameImage.height;
+
+          // Draw the frame on the canvas
+          context.drawImage(frameImage, 0, 0);
+
+          // Create an image object for the user-selected image
+          const userImage = new Image();
+          userImage.src = URL.createObjectURL(selectedImage);
+
+          userImage.onload = () => {
+              // Draw the user-selected image on top of the frame
+              context.drawImage(userImage, 0, 0, canvas.width, canvas.height);
+
+              // Convert the merged image to a data URL
+              const mergedImageURL = canvas.toDataURL('image/png');
+
+              // Display the download link
+              const downloadLink = document.getElementById('downloadLink');
+              downloadLink.href = mergedImageURL;
+              downloadLink.style.display = 'block';
+          };
+      };
   }
-  
-  // Function to save the selected frame
-  function saveAvatarFrame() {
-    const frameSelection = document.getElementById('frameSelection');
-    const selectedFrame = frameSelection.value;
-  
-    const avatarFrame = document.getElementById('avatar-frame');
-    avatarFrame.innerHTML = `
-        <img src="assets/images/splatfest/${selectedFrame}" alt="Avatar Frame" class="frame-image">
-        <img src="https://www.gravatar.com/avatar/5127de8a3a295a02ff3fc55bb6b2fabe?s=2048" alt="Cody's Avatar" class="avatar-image">
-    `;
-  
-    localStorage.setItem('selectedFrame', selectedFrame);
-
-      // Add a green success message
-  const successMessage = document.createElement('p');
-  successMessage.textContent = 'Selection successfully saved!';
-  successMessage.style.color = '#00C900';
-  document.getElementById('ChangeSplatfestFrameContent').appendChild(successMessage);
-
-  // Remove the success message after a few seconds
-  setTimeout(() => {
-    successMessage.remove();
-  }, 3000); // 3000 milliseconds (3 seconds)
-
 }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const storedFrame = localStorage.getItem('selectedFrame');
-    if (storedFrame) {
-        // Set the selected frame in the dropdown menu
-        setSelectedFrame(storedFrame);
-  
-        // Set the avatar frame
-        const avatarFrame = document.getElementById('avatar-frame');
-        avatarFrame.innerHTML = `
-            <img src="assets/images/splatfest/${storedFrame}" alt="Avatar Frame" class="frame-image">
-            <img src="https://www.gravatar.com/avatar/5127de8a3a295a02ff3fc55bb6b2fabe?s=2048" alt="Cody's Avatar" class="avatar-image">
-        `;
-    }
-  });
-  
-  // Function to display the preview of the selected avatar frame
-  function displayPreview() {
-    const frameSelection = document.getElementById('frameSelection');
-    const selectedFrame = frameSelection.value;
-    const previewImage = document.getElementById('preview-image');
-    
-    previewImage.src = `assets/images/splatfest/${selectedFrame}`;
-  
-    // Save the selected frame to localStorage
-    localStorage.setItem('selectedFramePreview', selectedFrame);
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    // Retrieve the previously selected frame from localStorage
-    const storedFrame = localStorage.getItem('selectedFramePreview');
-    if (storedFrame) {
-      // Set the selected frame in the dropdown menu
-      setSelectedFrame(storedFrame);
-  
-      // Display the preview for the stored frame
-      const previewImage = document.getElementById('preview-image');
-      previewImage.src = `assets/images/splatfest/${storedFrame}`;
-    }
-  });
-  
-  // Attach event listener to the dropdown menu to trigger the preview function
-  document.getElementById('frameSelection').addEventListener('change', displayPreview);
-    
