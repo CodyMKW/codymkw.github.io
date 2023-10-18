a9b287e4a761bfc8762c8053aa3a8354f8fb1752
CodyMKW <CodyMKW@users.noreply.github.com>
2023-10-18 18:44:18 +0000

    fn image fix

--- assets/javascript/randomfnmap.js
+++ assets/javascript/randomfnmap.js
@@ -2,13 +2,14 @@ const fortniteMapCodes = ["8440-2203-4683", "0389-9589-7317", "2057-3742-0636"];
 const fortniteMapImages = ["https://cdn-0001.qstv.on.epicgames.com/IqiigHsawdZQqYgXcJ/image/screen_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/wPHOYjXxYKCMUgkNZH/image/screen_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/SqCkzBhWpiNBSpAMRi/image/landscape_comp.jpeg"];  // Add your map images
 
 function generateRandomFortniteMap() {
-  const randomIndex = Math.floor(Math.random() * fortniteMapCodes.length);
-  const randomMapCode = fortniteMapCodes[randomIndex];
-  const randomMapImage = fortniteMapImages[randomIndex];
-
-  const mapInfoDiv = document.getElementById("fortniteMapInfo");
-  mapInfoDiv.innerHTML = `
-    <img src="${randomMapImage}" alt="Fortnite Map Image">
-    <p>Map Code: ${randomMapCode}</p>
-  `;
-}
+    const randomIndex = Math.floor(Math.random() * fortniteMapCodes.length);
+    const randomMapCode = fortniteMapCodes[randomIndex];
+    const randomMapImage = fortniteMapImages[randomIndex];
+  
+    const mapInfoDiv = document.getElementById("fortniteMapInfo");
+    mapInfoDiv.innerHTML = `
+      <img src="${randomMapImage}" alt="Fortnite Map Image" class="fortnite-map-image">
+      <p>Map Code: ${randomMapCode}</p>
+    `;
+  }
+  
