4ebe1c1335abdb9650c58c3bfa4adf1736ed3738
CodyMKW <CodyMKW@users.noreply.github.com>
2023-10-18 19:01:43 +0000

    added maps

--- assets/javascript/randomfnmap.js
+++ assets/javascript/randomfnmap.js
@@ -1,5 +1,5 @@
-const fortniteMapCodes = ["8440-2203-4683", "0389-9589-7317", "2057-3742-0636"];  // Add your map codes
-const fortniteMapImages = ["https://cdn-0001.qstv.on.epicgames.com/IqiigHsawdZQqYgXcJ/image/screen_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/wPHOYjXxYKCMUgkNZH/image/screen_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/SqCkzBhWpiNBSpAMRi/image/landscape_comp.jpeg"];  // Add your map images
+const fortniteMapCodes = ["8440-2203-4683", "0389-9589-7317", "2057-3742-0636", "4394-0704-4209", "8671-2143-9286", "9056-2612-1999", "1350-8290-6429", "3632-2839-9780", "3426-5561-3374"];  // Add your map codes
+const fortniteMapImages = ["https://cdn-0001.qstv.on.epicgames.com/IqiigHsawdZQqYgXcJ/image/screen_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/wPHOYjXxYKCMUgkNZH/image/screen_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/SqCkzBhWpiNBSpAMRi/image/landscape_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/kUMAFeilzxclSYHBna/image/landscape_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/YvwcDeLzhuXTTqSUZR/image/landscape_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/IwBjFGXjFoHUssYnxk/image/landscape_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/yvqFSXHRWyfRXMAlPa/image/landscape_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/CGXorfknJsaAkhGZgy/image/landscape_comp.jpeg", "https://cdn-0001.qstv.on.epicgames.com/QPbjeyZHnyFvYDxROK/image/landscape_comp.jpeg"];  // Add your map images
 
 function generateRandomFortniteMap() {
     const randomIndex = Math.floor(Math.random() * fortniteMapCodes.length);
