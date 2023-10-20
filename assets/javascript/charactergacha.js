39a22325e21869913598dcd20875269d8edf8609
CodyMKW <CodyMKW@users.noreply.github.com>
2023-10-20 19:02:02 +0000

    character batch update

--- assets/javascript/charactergacha.js
+++ assets/javascript/charactergacha.js
@@ -215,25 +215,30 @@ function generateCharacter() {
   characterInfoDiv.innerHTML = generateCharacterHTML(randomCharacter);
 }
 
-// Function to generate HTML for displaying a character batch
-function generateCharacterBatchHTML(characterBatch) {
-    let characterBatchHTML = '';
-    for (let character of characterBatch) {
-      characterBatchHTML += generateCharacterHTML(character);
-    }
-    return characterBatchHTML;
-  }
-  
-  // Function to generate a batch of 10 characters
-  function generateCharacterBatch() {
+// Function to generate a batch of 10 characters
+function generateCharacterBatch() {
     let characterBatch = [];
     for (let i = 0; i < 10; i++) {
       let randomIndex = Math.floor(Math.random() * characters.length);
       characterBatch.push(characters[randomIndex]);
     }
   
-    // Display the randomly selected characters
+    // Display the randomly selected characters in two rows
     let characterBatchDiv = document.getElementById("CharacterBatch");
     characterBatchDiv.innerHTML = generateCharacterBatchHTML(characterBatch);
   }
+  
+  // Function to generate HTML for displaying a character batch in two rows
+  function generateCharacterBatchHTML(characterBatch) {
+    let characterBatchHTML = '<div class="character-row">';
+    for (let i = 0; i < characterBatch.length; i++) {
+      if (i === 5) {
+        characterBatchHTML += '</div><div class="character-row">';
+      }
+      characterBatchHTML += generateCharacterHTML(characterBatch[i]);
+    }
+    characterBatchHTML += '</div>';
+    return characterBatchHTML;
+  }
+  
   
