dd746ed26599f36e4c49140d88e31754d315e185
CodyMKW <CodyMKW@users.noreply.github.com>
2023-10-20 06:47:00 +0000

    Gacha beta

--- assets/javascript/randompersonality.js
+++ assets/javascript/randompersonality.js
@@ -1,99 +1,99 @@
-// Personality Generator data
+// Character Gacha data
 var characters = [
     {
       name: "Mario (Nintendo)",
       description: "The iconic plumber from the Mushroom Kingdom, known for his red cap and overalls.",
-      image: "assets/images/personalitys/Mario.png"
+      image: "assets/images/characters/Mario.png"
     },
     {
       name: "Link (The Legend of Zelda)",
       description: "The courageous hero of Hyrule, often seen wielding a sword and shield.",
-      image: "assets/images/personalitys/Link.png"
+      image: "assets/images/characters/Link.png"
     },
     {
       name: "Samus Aran (Metroid)",
       description: "A skilled bounty hunter known for her power armor and her missions against space pirates.",
-      image: "assets/images/personalitys/Samus.png"
+      image: "assets/images/characters/Samus.png"
     },
     {
         name: "Pikachu (Pokémon)",
         description: "The electric-type Pokémon and mascot of the Pokémon franchise.",
-        image: "assets/images/personalitys/Pikachu.png"
+        image: "assets/images/characters/Pikachu.png"
     },
     {
         name: "Goku (Dragon Ball Z)",
         description: "A Saiyan warrior with immense power, known for his spiky hair and energy attacks.",
-        image: "assets/images/personalitys/Goku.png"
+        image: "assets/images/characters/Goku.png"
     },
     {
         name: "Naruto Uzumaki (Naruto)",
         description: "A ninja with dreams of becoming Hokage, recognized by his headband and orange jumpsuit.",
-        image: "assets/images/personalitys/Naruto.png"
+        image: "assets/images/characters/Naruto.png"
     },
     {
         name: "Luffy (One Piece)",
         description: "The rubber-powered pirate captain from the Straw Hat Pirates.",
-        image: "assets/images/personalitys/Luffy.png"
+        image: "assets/images/characters/Luffy.png"
     },
     {
         name: "Edward Elric (Fullmetal Alchemist)",
         description: "A talented alchemist with a metal arm and leg in search of the Philosopher's Stone.",
-        image: "assets/images/personalitys/Edward.png"
+        image: "assets/images/characters/Edward.png"
     },
     {
         name: "Light Yagami (Death Note)",
         description: "A high school student who gains the power to control life and death with the Death Note.",
-        image: "assets/images/personalitys/Light.png"
+        image: "assets/images/characters/Light.png"
     },
     {
         name: "Hermione Granger (Harry Potter)",
         description: "A brilliant witch and one of Harry Potter's closest friends at Hogwarts.",
-        image: "assets/images/personalitys/Hermione.png"
+        image: "assets/images/characters/Hermione.png"
     },
     {
         name: "Captain America (Marvel Cinematic Universe)",
         description: "The super-soldier and patriotic hero, wielding his iconic shield.",
-        image: "assets/images/personalitys/CapAmerica.png"
+        image: "assets/images/characters/CapAmerica.png"
     },
     {
         name: "Rey (Star Wars)",
         description: "A scavenger turned Jedi, seeking her place in the Force.",
-        image: "assets/images/personalitys/Rey.png"
+        image: "assets/images/characters/Rey.png"
     },
     {
         name: "Iron Man (Marvel Cinematic Universe)",
         description: "A genius billionaire, inventor, and philanthropist who becomes the armored hero, Iron Man.",
-        image: "assets/images/personalitys/IronMan.png"
+        image: "assets/images/characters/IronMan.png"
     },
     {
         name: "Black Panther (Marvel Cinematic Universe)",
         description: "The king of Wakanda, known for his advanced suit and enhanced abilities.",
-        image: "assets/images/personalitys/Panther.png"
+        image: "assets/images/characters/Panther.png"
     },
     {
         name: "Wonder Woman (DC Extended Universe)",
         description: "An Amazonian warrior princess with superhuman abilities.",
-        image: "assets/images/personalitys/WonderWoman.png"
+        image: "assets/images/characters/WonderWoman.png"
     },
     {
         name: "Harley Quinn (DC Extended Universe)",
         description: "The Joker's partner-in-crime, known for her chaotic and unpredictable nature.",
-        image: "assets/images/personalitys/Harley.png"
+        image: "assets/images/characters/Harley.png"
     },
     {
         name: "Totoro (My Neighbor Totoro)",
         description: "A friendly forest spirit from Studio Ghibli's 'My Neighbor Totoro'.",
-        image: "assets/images/personalitys/Totoro.png"
+        image: "assets/images/characters/Totoro.png"
     },
     {
         name: "WALL-E (WALL-E)",
         description: "A lovable robot designed to clean up a polluted Earth.",
-        image: "assets/images/personalitys/WALL-E.png"
+        image: "assets/images/characters/WALL-E.png"
     },
     {
         name: "Golden Mario (Nintendo)",
         description: "A special, golden version of the iconic plumber, Mario.",
-        image: "assets/images/personalitys/GoldenMario.png"
+        image: "assets/images/characters/GoldenMario.png"
     }
   ];
   
@@ -107,7 +107,7 @@ function generateCharacterHTML(character) {
 }
 
 // Update the Character Gacha info
-var characterInfoDiv = document.getElementById("PersonalityInfo");
+var characterInfoDiv = document.getElementById("CharacterInfo");
 
 function generateCharacter() {
   // Generate a random index to select a character
