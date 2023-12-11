547172b6109ccda932806dfd36ccb442a789b94b
CodyMKW <CodyMKW@users.noreply.github.com>
2023-12-11 03:27:47 -0500

    Update mostplayed.js

--- assets/javascript/mostplayed.js
+++ assets/javascript/mostplayed.js
@@ -1,38 +1,44 @@
-var clickCount = 0;
-
-// Sort games based on playtime in descending order
-gamesData.sort(function (a, b) {
-  return b.playtime - a.playtime;
-});
-
-// Function to generate the HTML for the cover art image
-function generateCoverArtHTML(imageUrl, altText) {
-  return '<img src="' + imageUrl + '" alt="' + altText + '" class="game-cover">';
-}
-
-// Update games table
-var gamesTable = document.querySelector('#games-table tbody');
+ // Most Played Games table info
+ var clickCount = 0;
 
 // Fetch the JSON file
-fetch('https://api.npoint.io/1c8c00be23c0d17cd8b5')
+fetch(' https://api.npoint.io/1c8c00be23c0d17cd8b5')
   .then(response => response.json())
   .then(data => {
-    // Sort the games based on playtime in descending order
+    // Sort games based on playtime in descending order
     data.sort((a, b) => b.playtime - a.playtime);
 
+    // Update games table
+    var gamesTable = document.querySelector('#games-table tbody');
+    
     // Clear existing rows
     gamesTable.innerHTML = '';
 
     // Create new rows based on the updated games data
-    data.forEach(function (game) {
+    data.forEach(game => {
       var row = document.createElement('tr');
-      row.innerHTML = '<td>' + generateCoverArtHTML(game.cover, game.name) + '</td><td><a href="' + game.link + '" target="_blank">' + game.name + '</a></td><td>' + formatPlaytime(game.playtime) + ' hours</td>';
+      row.innerHTML = `<td>${generateCoverArtHTML(game.cover, game.name)}</td><td><a href="${game.link}" target="_blank">${game.name}</a></td><td>${formatPlaytime(game.playtime)} hours</td>`;
       gamesTable.appendChild(row);
     });
   })
   .catch(error => console.error('Error fetching JSON:', error));
-
-// Function to format playtime with comma for thousands
-function formatPlaytime(playtime) {
-  return playtime.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+ 
+ // Sort games based on playtime in descending order
+ gamesData.sort(function (a, b) {
+   return b.playtime - a.playtime;
+ });
+ 
+ // Function to generate the HTML for the cover art image
+ function generateCoverArtHTML(imageUrl, altText) {
+   return '<img src="' + imageUrl + '" alt="' + altText + '" class="game-cover">';
+ }
+ 
+ // Function to format playtime with comma for thousands
+ function formatPlaytime(playtime) {
+   return playtime.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ }
+ 
+ // Add a tooltip with game name to the cover art image
+ function generateCoverArtHTML(coverURL, gameName) {
+   return '<img src="' + coverURL + '" alt="Cover Art" title="' + gameName + '" style="max-width: 70px; margin-right: 10px; margin-right: 10px; border-radius: 4px;">';
+ }
