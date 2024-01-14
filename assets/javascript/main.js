b9e4ed88e0c3b9753b02e49d76ed489a014e7e79
CodyMKW <CodyMKW@users.noreply.github.com>
2024-01-14 06:34:56 -0500

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,30 +1,30 @@
-  function showContent(page) {
+function showContent(page) {
     const contentTabs = document.querySelectorAll('.tabContent');
     contentTabs.forEach(tab => {
-      tab.style.display = 'none';
+        tab.style.display = 'none';
     });
-  
+
     if (page) {
-      const contentElement = document.getElementById(page + 'Content');
-      if (contentElement) {
-        contentElement.style.display = 'block';
-      } else {
-        // If the requested page doesn't exist, show a custom 404 message
-        const errorElement = document.getElementById('errorContent');
-        if (errorElement) {
-          errorElement.style.display = 'block';
+        const contentElement = document.getElementById(page + 'Content');
+        if (contentElement) {
+            contentElement.style.display = 'block';
+        } else {
+            // If the requested page doesn't exist, show a custom 404 message
+            const errorElement = document.getElementById('errorContent');
+            if (errorElement) {
+                errorElement.style.display = 'block';
+            }
         }
-      }
     } else {
-      // Default to the "About" tab if no specific page is provided
-      document.getElementById('aboutContent').style.display = 'block';
+        // Default to the "About" tab if no specific page is provided
+        document.getElementById('aboutContent').style.display = 'block';
     }
-  }
-  
-  // Parse the URL to get the page parameter
-  const urlParams = new URLSearchParams(window.location.search);
-  const pageParam = urlParams.get('page');
-  showContent(pageParam);
+}
+
+// Parse the URL to get the page parameter
+const path = window.location.pathname;
+const pageParam = path.slice(1); // Remove the leading slash
+showContent(pageParam);
   
   // Birthday code
 // Get the current date
