67e97602bf715acf906bc328b97b5882a61435f2
CodyMKW <CodyMKW@users.noreply.github.com>
2024-02-20 21:29:23 +0000

    decided to remove PWA note needed

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -175,14 +175,3 @@ function getOrdinalSuffix(number) {
      
      // Update the footer text
      footer.innerHTML = footerText;
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./codymkw-sw.js')
-    .then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-}
