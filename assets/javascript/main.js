29107d222cc35d331dd796635eca7befb9ac7dd2
CodyMKW <CodyMKW@users.noreply.github.com>
2024-02-20 16:13:31 -0500

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -34,9 +34,9 @@ function updatePresence() {
 
       // Update HTML content based on whether a game is being played or not
       if (gameName) {
-        statusContainer.innerHTML = `<p>Currently <span style="color: ${statusColor};">${onlineStatus.charAt(0)}${onlineStatus.slice(1).toLowerCase()}</span> playing ${gameName} <a href="#" id="check-switch-game-status">🔎</a></p>`;
+        statusContainer.innerHTML = `<p>Currently <span style="color: ${statusColor};">${onlineStatus.charAt(0)}${onlineStatus.slice(1).toLowerCase()}</span> playing ${gameName} <a id="check-switch-game-status">🔎</a></p>`;
       } else {
-        statusContainer.innerHTML = `<p>Currently <span style="color: ${statusColor};">${onlineStatus.charAt(0)}${onlineStatus.slice(1).toLowerCase()}</span> <a href="#" id="check-switch-game-status">🔎</a></p>`;
+        statusContainer.innerHTML = `<p>Currently <span style="color: ${statusColor};">${onlineStatus.charAt(0)}${onlineStatus.slice(1).toLowerCase()}</span> <a id="check-switch-game-status">🔎</a></p>`;
       }
 
       // Add event listener to the link to open modal
