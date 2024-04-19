3ad9f86827e721b70cfada1b480d3a1777379241
CodyMKW <CodyMKW@users.noreply.github.com>
2024-04-19 02:24:21 -0400

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -219,12 +219,12 @@ function openBetaModal() {
 
     // Add text indicating preview version and link to the main version
     const betaText = document.createElement('p');
-    betaText.innerHTML = '<h1 style="text-align: center;"><b><u>Preview Webpage</u></b></h1><br>This is a preview version of the webpage it gets updates to it for testing before being pushed to the main version. <br>Click ';
+    betaText.innerHTML = '<h1 style="text-align: center;"><b><u>Preview Webpage</u></b></h1><br>This is a preview version of the webpage. It gets updates for testing before being pushed to the main version.<br><div style="text-align: center;">Click ';
     const betaLink = document.createElement('a');
     betaLink.href = 'https://codymkw.github.io';
     betaLink.textContent = 'here';
     betaText.appendChild(betaLink);
-    betaText.innerHTML += ' to visit the main version.';
+    betaText.innerHTML += ' to visit the main version.</div>';
 
     modalContent.appendChild(closeButton);
     modalContent.appendChild(betaText);
