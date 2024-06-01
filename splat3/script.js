136e8f24a731bbc0c9bcd97f8aad42f887b25b14
CodyMKW <CodyMKW@users.noreply.github.com>
2024-06-01 19:13:56 -0400

    Update script.js

--- splat3/script.js
+++ splat3/script.js
@@ -17,6 +17,11 @@ let weapons = [];
 let headgears = [];
 let shirts = [];
 let shoes = [];
+let filteredAmiiboGear = {
+  headgears: [],
+  shirts: [],
+  shoes: []
+};
 
 // Fetch data from the JSON file
 fetch(apiUrl)
@@ -26,6 +31,7 @@ fetch(apiUrl)
     headgears = data.headgears;
     shirts = data.shirts;
     shoes = data.shoes;
+    filteredAmiiboGear = data.filteredAmiiboGear;
   })
   .catch(error => console.error('Error fetching data:', error));
 
@@ -69,85 +75,12 @@ closeButton.addEventListener('click', function() {
 function randomize() {
   const randomweapon = weapons[Math.floor(Math.random() * weapons.length)];
   let randomheadgear, randomshirt, randomshoe;
-  
+
   // Check if "Hide amiibo gear" is checked and filter out unwanted gear
   if (hideAmiiboGearCheckbox.checked) {
-    const filteredHeadgears = headgears.filter(gear => {
-      return ![
-        "Boss Floss",
-        "Chaos Helm",
-        "Enchanted Hat",
-        "Fresh Fish Head",
-        "Marinated Headphones",
-        "Pearlescent Crown L",
-        "Pearlescent Crown S",
-        "Power Mask Mk I",
-        "Power Mask",
-        "Samurai Helmet",
-        "Squid Clip-Ons",
-        "Squid Hairclip",
-        "Squinja Mask Mk I",
-        "Squinja Mask Mk II",
-        "Steel Helm",
-        "Sushi Sous-Chef Band",
-        "Armor Helmet Replica",
-        "Hero Headset Replica",
-        "Hohojiro Mask",
-        "Onaga Mask",
-        "Manta Mask"
-      ].includes(gear);
-    });
-    const filteredShirts = shirts.filter(gear => {
-      return ![
-        "Black-Belt Gi",
-        "Chaos Commander Suit",
-        "Enchanted Robe A",
-        "Enchanted Robe B",
-        "Fresh Fish Gloves",
-        "Marinated Top",
-        "Pearlescent Hoodie",
-        "Power Armor Mk I",
-        "Power Armor",
-        "Samurai Jacket",
-        "School Cardigan A",
-        "School Cardigan B",
-        "School Uniform A",
-        "School Uniform B",
-        "Schoolyard Scrap Jack",
-        "Squinja Suit",
-        "Steel Platemail",
-        "Armor Jacket Replica",
-        "Hero Jacket Replica",
-        "Chomp Top",
-        "Eelneck Tank",
-        "Big Slick"
-      ].includes(gear);
-    });
-    const filteredShoes = shoes.filter(gear => {
-      return ![
-        "Baggy-Sock Fringe Loafs",
-        "Base Fringed Loafers",
-        "Base School Shoes",
-        "Chaos Kicks",
-        "Enchanted Boots",
-        "Fresh Fish Feet",
-        "Kick Dampeners",
-        "Knotty Bois",
-        "Marinated Slip-Ons",
-        "Pearlescent Kicks",
-        "Power Boots Mk I",
-        "Power Boots",
-        "Samurai Shoes",
-        "School Shoes + Hi Socks",
-        "Squinja Boots",
-        "Steel Greaves",
-        "Armor Boot Replicas",
-        "Hero Runner Replicas",
-        "Faux Sharkskin Platfins",
-        "Eel Heel Socks",
-        "Big Muds"
-      ].includes(gear);
-    });
+    const filteredHeadgears = headgears.filter(gear => !filteredAmiiboGear.headgears.includes(gear));
+    const filteredShirts = shirts.filter(gear => !filteredAmiiboGear.shirts.includes(gear));
+    const filteredShoes = shoes.filter(gear => !filteredAmiiboGear.shoes.includes(gear));
     randomheadgear = filteredHeadgears[Math.floor(Math.random() * filteredHeadgears.length)];
     randomshirt = filteredShirts[Math.floor(Math.random() * filteredShirts.length)];
     randomshoe = filteredShoes[Math.floor(Math.random() * filteredShoes.length)];
@@ -156,7 +89,7 @@ function randomize() {
     randomshirt = shirts[Math.floor(Math.random() * shirts.length)];
     randomshoe = shoes[Math.floor(Math.random() * shoes.length)];
   }
-  
+
   weaponElement.textContent = randomweapon;
   headgearElement.textContent = randomheadgear;
   shirtElement.textContent = randomshirt;
@@ -171,8 +104,8 @@ tweetButton.addEventListener("click", function() {
   const shoe = document.getElementById("shoe").textContent;
   const tweetMessage = `I just generated a random weapon & outfit using the #Splatoon3 Randomizer!\n\nResults:\nWeapon - ${weapon}\nHeadgear - ${headgear}\nShirt - ${shirt}\nShoes - ${shoe}\nhttps://codymkw.github.io/splat3`;
   
-      navigator.clipboard.writeText(tweetMessage);
-      alert("The message was copied to clipboard now you can go to twitter and post it or you can post it somewhere else it's up to you! :D");
+  navigator.clipboard.writeText(tweetMessage);
+  alert("The message was copied to clipboard now you can go to twitter and post it or you can post it somewhere else it's up to you! :D");
 });
 
 randomizeButton.addEventListener('click', () => {
@@ -181,17 +114,17 @@ randomizeButton.addEventListener('click', () => {
 
 randomizeButton.addEventListener("click", randomize);
 
-   // Get the footer element
-   var footer = document.querySelector('footer');
+// Get the footer element
+var footer = document.querySelector('footer');
 
-   // Get the start year
-   var startYear = 2023; // Replace with the desired start year
-   
-   // Get the current year
-   var currentYear = new Date().getFullYear();
-   
-   // Build the footer text
-   var footerText = '&copy; ' + (startYear === currentYear ? startYear : startYear + ' - ' + currentYear) + ' CodyMKW. All rights reserved.';
-   
-   // Update the footer text
-   footer.innerHTML = footerText;
+// Get the start year
+var startYear = 2023; // Replace with the desired start year
+
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Build the footer text
+var footerText = '&copy; ' + (startYear === currentYear ? startYear : startYear + ' - ' + currentYear) + ' CodyMKW. All rights reserved.';
+
+// Update the footer text
+footer.innerHTML = footerText;
