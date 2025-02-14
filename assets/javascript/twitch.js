80eafb23b26d4b32630f9b1854ea0f04efbc32c8
CodyMKW <CodyMKW@users.noreply.github.com>
2025-02-14 06:43:35 +0000

    Twitch page test

---
+++ assets/javascript/twitch.js
@@ -0,0 +1,27 @@
+function getQueryParam() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("") || "";
+}
+
+function loadTwitchStream(username) {
+    if (!username) {
+        username = document.getElementById("twitch-username").value;
+    }
+    if (username) {
+        window.history.pushState({}, "", `?=${username}`);
+        document.getElementById("twitch-player").innerHTML = `
+            <iframe 
+                src="https://player.twitch.tv/?channel=${username}&parent=${window.location.hostname}" 
+                height="480" width="800" 
+                allowfullscreen>
+            </iframe>`;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const username = getQueryParam();
+    if (username) {
+        document.getElementById("twitch-username").value = username;
+        loadTwitchStream(username);
+    }
+});
