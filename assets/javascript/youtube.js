2ad4b02273243217e2cf2158c9083087d6c059d2
CodyMKW <CodyMKW@users.noreply.github.com>
2025-02-16 05:11:17 +0000

    hmm

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -44,7 +44,7 @@ function loadYouTubeVideos() {
     const iframe = document.createElement("iframe");
     iframe.width = "100%";
     iframe.height = "100%";
-    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
     iframe.frameBorder = "0";
     iframe.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
     iframe.allowFullscreen = true;
